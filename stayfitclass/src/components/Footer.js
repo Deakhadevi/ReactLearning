e8e4038ce0914a86bb096ee131d63dd7e9ce3695
psdurgadevi@gmail.com <psdurgadevi@gmail.com>
2022-12-31 22:32:21 +0530

    Durgadevi | created a new blog using class components as per feedback

---
+++ stayfitclass/src/components/Footer.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+ 
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <> 
+     <div>
+        <center> 
+            <h href="#" class="fa fa-facebook" />
+            <h href="#" class="fa fa-twitter" />
+            <h href="#" class="fa fa-google" />
+            <h href="#" class="fa fa-linkedin" />  
+        </center> 
+    
+    </div>
+    </>
+       
+    )
+  }
+}
+
